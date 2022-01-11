8273ab3296141a6cdb1b2f1b58781de32549e079
Liam <liam@enterpriseautomation.co.uk>
2022-01-11 09:55:44 +0000

    added http-server run to expect solution test

--- group_task/expect-sshpass-solutions-test.sh
+++ group_task/expect-sshpass-solutions-test.sh
@@ -5,8 +5,8 @@ read -s password
 
 ssh-keygen -R 192.168.1.173
 
-#expect -c "spawn ssh root@192.168.1.173 -oStrictHostKeyChecking=no; expect \"password:\"; send \"$password\r\"; interact"
+expect -c "spawn ssh root@192.168.1.173 -oStrictHostKeyChecking=no \"pkill http-server; pm2 start http-server\"; expect \"password:\"; send \"$password\r\"; interact"
 
-sshpass -p $password ssh root@192.168.1.173 -oStrictHostKeyChecking=no "pkill http-server; npm install pm2 -g; pm2 start http-server; echo 'server running'"
+#sshpass -p $password ssh root@192.168.1.173 -oStrictHostKeyChecking=no "pkill http-server; npm install pm2 -g; pm2 start http-server; echo 'server running'"
 
 echo "Yeah this works fine"
